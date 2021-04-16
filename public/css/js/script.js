0e0da3b2bdd40585323b62b02e67225164918846
MeHigh22 <pmihai31@gmail.com>
2021-04-16 14:50:42 +0200

    Bang

---
+++ public/css/js/script.js
@@ -0,0 +1,24 @@
+// let prenom = prompt("Quel est ton prénom?")
+
+// alert("Bonjour" + prenom );
+
+// let age = prompt("Tu as quel âge?")
+
+// alert("Tu as" + " " + age + " " + "ans");
+
+// let prenom = prompt("Quel est ton prénom?");
+// let nom = prompt("Quel est ton nom?");
+
+// let fullName = prenom +" " + nom;
+
+// alert(fullName);
+
+let prenom = prompt("Quel est ton prénom?");
+let nom = prompt("Quel est ton nom?");
+
+prenom = prenom.toUpperCase();
+
+let fullName = prenom +" "+ nom ;
+
+alert("Bonjour" +" " + fullName);
+
